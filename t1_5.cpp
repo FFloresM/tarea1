bde0f624fd4dd461d6b7324dbd2f643ee430e17f
Francisco Flores <ffloresm@inf.udec.cl>
2019-04-20 02:09:44 -0400

    nlogn

--- t1_5.cpp
+++ t1_5.cpp
@@ -6,6 +6,19 @@ using namespace std;
 
 int _i,_j;
 
+int binarySearch(vector<int> v, int l, int r, int x){
+	int mid;
+	if(r>=l)
+		mid = (l+r)/2;
+	if(v[mid]==x)
+		return mid;
+	if(v[mid]>x)
+		return binarySearch(v,l,mid-1,x);
+	else 
+		return -1;		//no encontrado
+	return binarySearch(v,mid+1,r,x);
+}
+
 void n2(vector<int> v, int x){
 	for (int i = 0; i < v.size(); ++i){
 		for (int j = 0; j < v.size() && i!=j; ++j){
@@ -18,8 +31,20 @@ void n2(vector<int> v, int x){
 	}
 }
 
+//fix it
 void nlogn(vector<int> v, int x){
-	/* code here */
+	int b,y;
+	for (int i = 0; i < v.size(); ++i){
+		b = v[i];
+		y = x-b;
+		_j = binarySearch(v,0,v.size()-1,y);
+		if (_j != -1)
+			continue;
+		else{
+			_i=i;
+			break;
+		}
+	}
 }
 
 void ene(vector<int> v, int x){
@@ -31,11 +56,13 @@ int main(int argc, char const *argv[])
 	//int n = atoi(argv[1]);
 	int myints[] = {1,2,3,4,5};
   	vector<int> v (myints, myints + sizeof(myints) / sizeof(int) );
+  	//vector<int> v (myints.begin(), myints.end());
 
-	int x = 5;
-	ene2(v,x);
+	int x = 7;
+	nlogn(v,x);
 	cout << _i << " " << _j << endl;
-	
+
+//	cout << binarySearch(v, 0, 4,x) << endl;	
 
 	return 0;
 }
