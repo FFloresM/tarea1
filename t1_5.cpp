5af960cf064d4e52726236152baa0c5d19fbe0df
Francisco Flores <ffloresm@inf.udec.cl>
2019-04-21 22:59:23 -0400

    n2 y nlogn listos

--- t1_5.cpp
+++ t1_5.cpp
@@ -15,11 +15,12 @@ void show(vector<int> v){
 	
 }
 
+
 //crea elementos aleatorios y los ordena
 void init(vector<int> &v, int len){
 	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 	default_random_engine generator(seed);
-	uniform_int_distribution<int> dist(1,100);
+	uniform_int_distribution<int> dist(1,10);
 	for (int i = 0; i < len; ++i){
 		v.push_back (dist(generator));
 	}
@@ -30,25 +31,26 @@ int binarySearch(vector<int> v, int l, int r, int x){
 	int mid;
 	if(r>=l)
 		mid = (l+r)/2;
+	else 
+		return -1;		//no encontrado
 	if(v[mid]==x)
 		return mid;
 	if(v[mid]>x)
 		return binarySearch(v,l,mid-1,x);
-	else 
-		return -1;		//no encontrado
+	
 	return binarySearch(v,mid+1,r,x);
 }
 
 void n2(vector<int> v, int x){
 	for (int i = 0; i < v.size(); ++i){
-		for (int j = 0; j < v.size() && i!=j; ++j){
-			if (v[i]+v[j]==x){
+		for (int j = 0; j < v.size() ; ++j){
+			if (v[i]+v[j]==x && i!=j){
 				_i=i;_j=j;
-				//cout << i << " "<< j << endl;
 				return;
 			}
 		}
 	}
+	_i=-1;_j=-1;
 }
 
 //fix it
@@ -58,13 +60,16 @@ void nlogn(vector<int> v, int x){
 		b = v[i];
 		y = x-b;
 		_j = binarySearch(v,0,v.size()-1,y);
-		if (_j == -1)
+		if (_j == -1 || i==_j){
+			_i=-1;_j=-1;
 			continue;
+		}	
 		else{
 			_i=i;
 			break;
 		}
 	}
+	
 }
 
 void ene(vector<int> v, int x){
@@ -75,20 +80,28 @@ int main(int argc, char const *argv[])
 {
 	//int n = atoi(argv[1]);
 	int myints[] = {1,2,3,4,5};
-  	vector<int> v;// (myints, myints + sizeof(myints) / sizeof(int) );
-  	//vector<int> v (myints.begin(), myints.end());
-/*
-	int x = 7;
+  	vector<int> v;
+
+  	init(v,20);
+	int x = 20;
+	show(v);
+
 	n2(v,x);
-	cout <<"n2: " << _i << " " << _j << endl;
+	if(_i>=0 && _j >=0 ){
+		cout<<"nlogn:"<<" A[" << _i<<"]" << " A[" << _j <<"]"<< endl;
+		cout << v[_i] <<" + "<< v[_j] <<" = " << x << endl;
+	}else cout << "valores no encontrados\n";
 
 	nlogn(v,x);
-	cout<<"nlogn: " << _i << " " << _j << endl;
-*/
+	if(_i>=0 && _j >=0 ){
+		cout<<"nlogn:"<<" A[" << _i<<"]" << " A[" << _j <<"]"<< endl;
+		cout << v[_i] <<" + "<< v[_j] <<" = " << x << endl;
+	}else cout << "valores no encontrados\n";
+	
+/*
   	init(v,20);
   	show(v);
-  	
-//	cout << binarySearch(v, 0, 4,x) << endl;	
+*/  		
 
 	return 0;
 }
