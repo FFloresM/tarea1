1916c8ff065e89a05114e30911a62c4aa38c5a25
Francisco Flores <ffloresm@inf.udec.cl>
2019-04-22 01:11:37 -0400

    3 alg plus time

--- t1_5.cpp
+++ t1_5.cpp
@@ -73,30 +73,60 @@ void nlogn(vector<int> v, int x){
 }
 
 void ene(vector<int> v, int x){
-	/* code here */
+	int l = 0;
+	int r = v.size()-1;
+	while(l < r){
+		if (v[l]+v[r] == x){
+			_i=l;_j=r;
+			return;
+		}else if(v[l]+v[r] < x)
+			l++;
+		else r--;
+	}
+	_i=-1;_j=-1;
 }
+
 
 int main(int argc, char const *argv[])
 {
 	//int n = atoi(argv[1]);
-	int myints[] = {1,2,3,4,5};
+
   	vector<int> v;
 
   	init(v,20);
-	int x = 20;
+	int x = 14;
 	show(v);
 
+	auto s1 = chrono::high_resolution_clock::now();
 	n2(v,x);
+	auto f1 = chrono::high_resolution_clock::now();
 	if(_i>=0 && _j >=0 ){
 		cout<<"nlogn:"<<" A[" << _i<<"]" << " A[" << _j <<"]"<< endl;
 		cout << v[_i] <<" + "<< v[_j] <<" = " << x << endl;
 	}else cout << "valores no encontrados\n";
+	auto d1 = chrono::duration_cast<chrono::nanoseconds> (f1 - s1).count();
+	cout <<"total time "<< d1 << " [ns]" << " \n\n";
 
+	auto s2 = chrono::high_resolution_clock::now();
 	nlogn(v,x);
+	auto f2 = chrono::high_resolution_clock::now();
 	if(_i>=0 && _j >=0 ){
 		cout<<"nlogn:"<<" A[" << _i<<"]" << " A[" << _j <<"]"<< endl;
 		cout << v[_i] <<" + "<< v[_j] <<" = " << x << endl;
 	}else cout << "valores no encontrados\n";
+	auto d2 = chrono::duration_cast<chrono::nanoseconds> (f2 - s2).count();
+	cout <<"total time "<< d2 << " [ns]" << " \n\n";
+
+	auto s3 = chrono::high_resolution_clock::now();
+	ene(v,x);
+	auto f3 = chrono::high_resolution_clock::now();
+	if(_i>=0 && _j >=0 ){
+		cout<<"nlogn:"<<" A[" << _i<<"]" << " A[" << _j <<"]"<< endl;
+		cout << v[_i] <<" + "<< v[_j] <<" = " << x << endl;
+	}else cout << "valores no encontrados\n";
+	auto d3 = chrono::duration_cast<chrono::nanoseconds> (f3 - s3).count();
+	cout <<"total time "<< d3 << " [ns]" << " \n\n";
+
 	
 /*
   	init(v,20);
